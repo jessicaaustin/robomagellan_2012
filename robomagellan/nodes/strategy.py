3a87d84c91a1a74b64aa8fb787a5f0037ea0b495
Jessica Austin <livermouse@gmail.com>
2012-03-21 14:13:04 -0500

    subscribe to /odom and /cone_coord topics

--- robomagellan/nodes/strategy.py
+++ robomagellan/nodes/strategy.py
@@ -5,22 +5,43 @@
 strategy
 - determines where to move the robot next
 
+listens to:
+ /odom
+ /cone_coord
+
+publishes to:
+ /move_base_simple/goal
+
 """
 
 import roslib; roslib.load_manifest('robomagellan')
 import rospy
 
 from waypoint_reader import WaypointFileReader
+from geometry_msgs.msg import PointStamped
+
+from nav_msgs.msg import Odometry
 
 import os
 import sys
 
 class Strategizer():
     def __init__(self, waypoints_file):
+        self.current_pos = None
+        self.cone_coord = None
         waypoint_file_reader = WaypointFileReader()
         self.waypoints = waypoint_file_reader.read_file(waypoints_file)
         rospy.loginfo("Strategizer loaded with waypoints:\n %s" % self.waypoints)
 
+    def setup_odom_callback(self):
+        def odom_callback(data):
+            self.current_pos = data.pose.pose.position
+        return odom_callback
+
+    def setup_cone_coord_callback(self):
+        def cone_coord_callback(data):
+            self.cone_coord = data.point
+        return cone_coord_callback
 
 if __name__ == '__main__':
     rospy.init_node('strategy')
@@ -33,6 +54,8 @@ if __name__ == '__main__':
     else:
         waypoints_file = sys.argv[1]
         strategizer = Strategizer(waypoints_file)
+        rospy.Subscriber('odom', Odometry, strategizer.setup_odom_callback())
+        rospy.Subscriber('cone_coord', PointStamped, strategizer.setup_cone_coord_callback())
 
     rate = rospy.Rate(10.0)
     while not rospy.is_shutdown():
