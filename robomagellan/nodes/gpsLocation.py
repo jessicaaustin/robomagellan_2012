43dae34f497465e778d5924c7682bfce0ec18bf5
Jessica Austin <livermouse@gmail.com>
2012-05-18 13:03:50 -0500

    get gps module publishing orientation in radians instead of degrees

--- robomagellan/nodes/gpsLocation.py
+++ robomagellan/nodes/gpsLocation.py
@@ -6,10 +6,6 @@
 # Uses pyproj to convert geographic coords (lat,lng) to local map coords (x,y)
 #   see http://code.google.com/p/pyproj/
 #
-#
-# TODO publish in radians instead of degrees. should go from -pi to pi, with 
-#      positive orientation being counterclockwise (z-up)
-#
 
 import roslib; roslib.load_manifest('robomagellan')
 
@@ -17,11 +13,8 @@ import sys
 import math
 from subprocess import Popen, PIPE, STDOUT
 from pyproj import Proj
-import os
 
 import rospy
-from geometry_msgs.msg import Twist
-from geometry_msgs.msg import Vector3
 from nav_msgs.msg import Odometry
 from std_msgs.msg import String
 
@@ -30,6 +23,7 @@ import settings
 
 class GpsLocation():
     def __init__(self):
+        rospy.loginfo("Initializing GpsLocation")
         rospy.loginfo('projection zone=%d' % settings.UTM_ZONE)
         self.projection = Proj({'proj':'utm', 'zone':settings.UTM_ZONE, 'ellps':'WGS84'})
         self.init_x = None
@@ -38,6 +32,11 @@ class GpsLocation():
 
     def knot_to_vel(self, num):
         return settings.KNOT_TO_M_S * num
+
+    # gps module gives us degrees from 0-360
+    # here we convert to radians, from -pi to pi, positive counterclockwise (z-up)
+    def heading_in_degrees_to_radians(self, heading):
+        return -1 * (heading - 180) * math.pi / 180
 
     def lat_lng_to_course_frame(self, lat,lng):
         return self.projection(lat,lng)
@@ -66,30 +65,34 @@ class GpsLocation():
         odom.pose.pose.position.x = updated_x
         odom.pose.pose.position.y = updated_y
         odom.twist.twist.linear.x = self.knot_to_vel(vel)
-        # TODO convert to radians. right now it's in degrees, but navigation node expects radians
-        odom.pose.pose.orientation.z = heading
+        odom.pose.pose.orientation.z = self.heading_in_degrees_to_radians(heading)
 
         self.publisher.publish(odom)
 
 
 if __name__ == '__main__':
-    CMD = 'gpsbabel -T -i garmin -f usb: -o nmea -F -'
-    if len(sys.argv) > 1:
-        CMD = sys.argv[1]
-    
-    rospy.loginfo('Running gps with command: %s' % CMD)
+    # wait for rxconsole to boot up
+    rospy.sleep(3)
+    CMD = "gpsbabel -T -i garmin -f usb: -o nmea -F -"
+#    rospy.loginfo("CMD: %s" % CMD)
+#    if len(sys.argv) > 1:
+#        CMD = sys.argv[1]
+#        rospy.loginfo("Updating CMD using given argument: %s" % CMD)
 
     rospy.init_node('gps_location')
     gps_location = GpsLocation()
     pub_raw = rospy.Publisher('gps_raw_data', String)
-
+    rate = rospy.Rate(10.0)
+    
+    rospy.loginfo("Running gps with command: %s" % CMD)
     p = Popen(CMD, stdout = PIPE, stderr = STDOUT, shell = True)
     while not rospy.is_shutdown():
         line = p.stdout.readline()
         if not line:
             rospy.logerr("Could not read from GPS!")
-            break
-        pub_raw.publish(line)
-        if "GPRMC" in line:
-            gps_location.publish_location(line)
+        else:
+            pub_raw.publish(line)
+            if "GPRMC" in line:
+                gps_location.publish_location(line)
+        rate.sleep()
 
