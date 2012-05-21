0b5c2755755db71bf42c4a805bb1a52660f01e9c
Bill Mania <maniabill@yahoo.com>
2012-05-21 15:14:49 -0500

    updated the frame ids and added some encoder stuff

--- robomagellan/nodes/wheel_encoders.py
+++ robomagellan/nodes/wheel_encoders.py
@@ -1,65 +1,113 @@
-
 # NOT A FUNCTIONING NODE... JUST NOTES FOR NOW   
- 
-# from rooma_sensor_handler.py
 
-    self._last_encoder_counts = None
 
-  def _normalize_encoder_count(self, count_delta, maximal_count):
-    if count_delta >= maximal_count / 2:
-      return count_delta - maximal_count + 1
-    elif count_delta <= -maximal_count / 2:
-      return count_delta + maximal_count + 1
-    return count_delta
 
-    # The distance and angle calculation sent by the robot seems to
-    # be really bad. Re-calculate the values using the raw enconder
-    # counts.
-    if self._last_encoder_counts:
-      count_delta_left = self._normalize_encoder_count(
-          self.encoder_counts_left - self._last_encoder_counts[0], 0xffff)
-      count_delta_right = self._normalize_encoder_count(
-          self.encoder_counts_right - self._last_encoder_counts[1], 0xffff)
-      distance_left = count_delta_left * self.ROOMBA_PULSES_TO_M
-      distance_right = count_delta_right * self.ROOMBA_PULSES_TO_M
-      self.distance = (distance_left + distance_right) / 2.0
-      self.angle = (distance_right - distance_left) / robot_types.ROBOT_TYPES['roomba'].wheel_separation
-    else:
-      self.disance = 0
-      self.angle = 0
-    self._last_encoder_counts = (self.encoder_counts_left, self.encoder_counts_right)
+#!/usr/bin/env python
 
- 
+"""Phidgets dual encoders
 
-# from turtlebot_node.py
-    def compute_odom(self, sensor_state, last_time, odom):
+"""
+
+import roslib; roslib.load_manifest('robomagellan')
+import rospy
+
+from ctypes import *
+from Phidgets.Devices.Encoder import Encoder
+from Phidgets.PhidgetException import PhidgetException
+from nav_msgs.msg import Odometry
+
+class PhidgetEncoders:
+
+    def __init__(self):
+        self.leftEncoder = 0
+        self.rightEncoder = 1
+
+        self.previousXPosition = 0
+        self.previousYPosition = 0
+
+        self.encoderPublisher = rospy.Publisher('odom', Odometry)
+
+        self.encoder = Encoder()
+
+        self.encoder.setOnAttachHandler(self.mcAttached)
+        self.encoder.setOnDetachHandler(self.mcDetached)
+        self.encoder.setOnErrorhandler(self.mcError)
+        self.encoder.setOnPositionChangeHandler(self.encoderPositionChange)
+
+        try:
+            self.encoder.openPhidget()
+
+        except PhidgetException, e:
+            rospy.logerror("openPhidget() failed")
+            rospy.logerror("code: %d" % e.code)
+            rospy.logerror("message", e.message)
+
+            raise
+
+        try:
+            self.encoder.waitForAttach(10000)
+
+        except PhidgetException, e:
+            rospy.logerror("waitForAttach() failed")
+            rospy.logerror("code: %d" % e.code)
+            rospy.logerror("message", e.message)
+    
+            raise
+
+    def encoderPositionChange(self, e):
+        """Called each time the encoder reports a change to its position.
+           This method will convert the encoder values to an updated
+           position in the base_link frame and then publish an Odometry
+           message to the odom topic.
         """
-        Compute current odometry.  Updates odom instance and returns tf
-        transform. compute_odom() does not set frame ids or covariances in
-        Odometry instance.  It will only set stamp, pose, and twist.
 
-        @param sensor_state: Current sensor reading
-        @type  sensor_state: TurtlebotSensorState
-        @param last_time: time of last sensor reading
-        @type  last_time: rospy.Time
-        @param odom: Odometry instance to update.
-        @type  odom: nav_msgs.msg.Odometry
+        source = e.device
+        rospy.logdebug("Encoder %i: Encoder %i -- Change: %i -- Time: %i -- Position: %i" % (source.getSerialNum(), e.index, e.positionChange, e.time, encoder.getPosition(e.index)))
 
-        @return: transform
-        @rtype: ( (float, float, float), (float, float, float, float) )
-        """
+        #
+        # we need from the encoder:
+        #
+        # which wheel encoder changed
+        # what is the new value for that encoder
+        # at what time was the encoder sampled
+        #
+        # we must:
+        #
+        # initialize the previous encoder value and timestamp for each
+        #  encoder
+        #
+        # calculate the difference between the current and previous
+        #  encoder and time values
+        #
+
+        if count_delta >= maximal_count / 2:
+          return count_delta - maximal_count + 1
+        elif count_delta <= -maximal_count / 2:
+          return count_delta + maximal_count + 1
+        return count_delta
+
+        # The distance and angle calculation sent by the robot seems to
+        # be really bad. Re-calculate the values using the raw enconder
+        # counts.
+        if self._last_encoder_counts:
+          count_delta_left = self._normalize_encoder_count(
+              self.encoder_counts_left - self._last_encoder_counts[0], 0xffff)
+          count_delta_right = self._normalize_encoder_count(
+              self.encoder_counts_right - self._last_encoder_counts[1], 0xffff)
+          distance_left = count_delta_left * self.ROOMBA_PULSES_TO_M
+          distance_right = count_delta_right * self.ROOMBA_PULSES_TO_M
+          self.distance = (distance_left + distance_right) / 2.0
+          self.angle = (distance_right - distance_left) / robot_types.ROBOT_TYPES['roomba'].wheel_separation
+        else:
+          self.distance = 0
+          self.angle = 0
+        self._last_encoder_counts = (self.encoder_counts_left, self.encoder_counts_right)
+
+
         # based on otl_roomba by OTL <t.ogura@gmail.com>
 
         current_time = sensor_state.header.stamp
         dt = (current_time - last_time).to_sec()
-
-        # On startup, Create can report junk readings
-        if abs(sensor_state.distance) > 1.0 or abs(sensor_state.angle) > 1.0:
-            raise Exception("Distance, angle displacement too big, invalid readings from robot. Distance: %.2f, Angle: %.2f" % (sensor_state.distance, sensor_state.angle))
-
-        # this is really delta_distance, delta_angle
-        d  = sensor_state.distance * self.odom_linear_scale_correction #correction factor from calibration
-        angle = sensor_state.angle * self.odom_angular_scale_correction #correction factor from calibration
 
         x = cos(angle) * d
         y = -sin(angle) * d
@@ -70,30 +118,49 @@
         self._pos2d.theta += angle
 
         # Turtlebot quaternion from yaw. simplified version of tf.transformations.quaternion_about_axis
-        odom_quat = (0., 0., sin(self._pos2d.theta/2.), cos(self._pos2d.theta/2.))
+        # odom_quat = (0., 0., sin(self._pos2d.theta/2.), cos(self._pos2d.theta/2.))
 
-        # construct the transform
-        transform = (self._pos2d.x, self._pos2d.y, 0.), odom_quat
 
         # update the odometry state
-        odom.header.stamp = current_time
-        odom.pose.pose   = Pose(Point(self._pos2d.x, self._pos2d.y, 0.), Quaternion(*odom_quat))
-        odom.twist.twist = Twist(Vector3(d/dt, 0, 0), Vector3(0, 0, angle/dt))
+    
+        encoder_odometry = Odometry()
+
+        encoder_odometry.header.stamp = current_time
+        encoder_odometry.pose.pose   = Pose(Point(self._pos2d.x, self._pos2d.y, 0.), Quaternion(*odom_quat))
+        encoder_odometry.twist.twist = Twist(Vector3(d/dt, 0, 0), Vector3(0, 0, angle/dt))
         if sensor_state.requested_right_velocity == 0 and \
                sensor_state.requested_left_velocity == 0 and \
                sensor_state.distance == 0:
-            odom.pose.covariance = ODOM_POSE_COVARIANCE2
-            odom.twist.covariance = ODOM_TWIST_COVARIANCE2
+            encoder_odometry.pose.covariance = ODOM_POSE_COVARIANCE2
+            encoder_odometry.twist.covariance = ODOM_TWIST_COVARIANCE2
         else:
-            odom.pose.covariance = ODOM_POSE_COVARIANCE
-            odom.twist.covariance = ODOM_TWIST_COVARIANCE
+            encoder_odometry.pose.covariance = ODOM_POSE_COVARIANCE
+            encoder_odometry.twist.covariance = ODOM_TWIST_COVARIANCE
 
-        # return the transform
-        return transform
+        return
 
-    def publish_odometry_transform(self, odometry):
-        self.transform_broadcaster.sendTransform(
-            (odometry.pose.pose.position.x, odometry.pose.pose.position.y, odometry.pose.pose.position.z),
-            (odometry.pose.pose.orientation.x, odometry.pose.pose.orientation.y, odometry.pose.pose.orientation.z,
-             odometry.pose.pose.orientation.w),
-             odometry.header.s
+    def mcAttached(self, e):
+        return
+    
+    def mcDetached(self, e):
+        return
+    
+    def mcError(self, e):
+        return
+    
+    def mcCurrentChanged(self, e):
+        return
+    
+    def mcInputChanged(self, e):
+        return
+    
+    def mcVelocityChanged(self, e):
+        return
+
+if __name__ == "__main__":
+    encoder = PhidgetEncoders()
+
+    while not rospy.is_shutdown():
+        rospy.spin()
+
+
