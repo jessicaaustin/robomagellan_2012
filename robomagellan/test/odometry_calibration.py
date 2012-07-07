46304ad9d35c5aa08e39d03ea8cdd7892697f0a1
Bill Mania <maniabill@yahoo.com>
2012-07-07 16:34:37 -0500

    running odom calibration node

--- robomagellan/test/odometry_calibration.py
+++ robomagellan/test/odometry_calibration.py
@@ -1,6 +1,6 @@
 
-rostopic pub -1 /cmd_vel geometry_msgs/Twist '[0.8, 0, 0]' '[0, 0, 0.0]'
+#rostopic pub -1 /cmd_vel geometry_msgs/Twist '[0.8, 0, 0]' '[0, 0, 0.0]'
 #rostopic pub -1 /cmd_vel geometry_msgs/Twist '[-0.33, 0, 0]' '[0, 0, 0.0]'
-#rostopic pub -1 /cmd_vel geometry_msgs/Twist '[0.0, 0, 0]' '[0, 0, -1.0]'
+rostopic pub -1 /cmd_vel geometry_msgs/Twist '[0.0, 0, 0]' '[0, 0, -5.0]'
 
 rostopic pub -1 /cmd_vel geometry_msgs/Twist '[0.0, 0, 0]' '[0, 0, 0.0]'
--- robomagellan/test/odometry_debug.py
+++ robomagellan/test/odometry_debug.py
@@ -8,32 +8,40 @@ import tf
 from sensor_msgs.msg import Imu
 from nav_msgs.msg import Odometry
 
-def setup_odom_callback():
-    def odom_callback(data):
-        odom = data
-        (x,y) = (odom.pose.pose.position.x, odom.pose.pose.position.y)
-        yaw = tf.transformations.euler_from_quaternion(odom.pose.pose.orientation)[2]
-        print "odom (x,y) = (%f, %f) / yaw = %f" % (x,y,yaw)
-    return setup_odom_callback
+odom = (0, 0, 0)
+wheel_odom = (0, 0, 0)
+imu = (0, 0, 0)
 
-def setup_wheel_odom_callback():
-    def odom_callback(data):
-        odom = data
-        (x,y) = (odom.pose.pose.position.x, odom.pose.pose.position.y)
-        yaw = tf.transformations.euler_from_quaternion(odom.pose.pose.orientation)[2]
-        print "wheel_odom (x,y) = (%f, %f) / yaw = %f" % (x,y,yaw)
-    return setup_odom_callback
+def print_results():
+    print odom, wheel_odom, imu
 
-def setup_imu_callback():
-    def imu_callback(data):
-        imu = data
-        yaw = tf.transformations.euler_from_quaternion(imu.orientation)[2]
-        print "imu / yaw = %f" % yaw
-    return setup_odom_callback
+def odom_callback(data):
+    odom = data
+    (x,y) = (odom.pose.pose.position.x, odom.pose.pose.position.y)
+    o = odom.pose.pose.orientation
+    yaw = tf.transformations.euler_from_quaternion((o.x, o.y, o.z, o.w))[2]
+    odom = (x,y,yaw)
+    print_results()
+
+def wheel_odom_callback(data):
+    odom = data
+    (x,y) = (odom.pose.pose.position.x, odom.pose.pose.position.y)
+    o = odom.pose.pose.orientation
+    yaw = tf.transformations.euler_from_quaternion((o.x, o.y, o.z, o.w))[2]
+    wheel_odom = (x,y,yaw)
+    print_results()
+
+def imu_callback(data):
+    imu = data
+    o = imu.orientation
+    yaw = tf.transformations.euler_from_quaternion((o.x, o.y, o.z, o.w))[2]
+    imu = (0,0,yaw)
+    print_results()
 
 if __name__ == '__main__':
     rospy.init_node('odometry_debug')
-    rospy.Subscriber('wheel_odom', Odometry, setup_wheel_odom_callback())
-    rospy.Subscriber('imu_data', Imu, setup_imu_callback())
-    rospy.Subscriber('odom', Odometry, setup_odom_callback())
+    rospy.Subscriber('wheel_odom', Odometry, wheel_odom_callback)
+    rospy.Subscriber('imu_data', Imu, imu_callback)
+    rospy.Subscriber('odom', Odometry, odom_callback)
 
+    rospy.spin()
