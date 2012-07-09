1e80e03d4d7968babe6d03f47279b52107ff5ce0
Jessica Austin <livermouse@gmail.com>
2012-07-08 19:02:13 -0500

    update odometry debug node

--- robomagellan/test/odometry_debug.py
+++ robomagellan/test/odometry_debug.py
@@ -7,36 +7,39 @@ import tf
 
 from sensor_msgs.msg import Imu
 from nav_msgs.msg import Odometry
+from geometry_msgs.msg import Twist
 
-odom = (0, 0, 0)
-wheel_odom = (0, 0, 0)
-imu = (0, 0, 0)
+cmd_vel = [('cmd_vel_time', 'cmd_vel_x', 'cmd_vel_yaw')]
+imu = [('imu_time','imu_yaw')]
+wheel_odom = [('wheel_odom_time', 'wheel_odom_x', 'wheel_odom_y', 'wheel_odom_yaw')]
+odom = [('odom_time', 'odom_x', 'odom_y', 'odom_yaw')]
 
 def print_results():
-    print odom, wheel_odom, imu
+    print cmd_vel
+    print imu
+    print wheel_odom
+    print odom
+
+def cmd_vel_callback(data):
+    (x,yaw) = (data.linear.x, data.angular.z)
+    cmd_vel.append((rospy.Time.now(), x, yaw))
 
 def odom_callback(data):
-    odom = data
-    (x,y) = (odom.pose.pose.position.x, odom.pose.pose.position.y)
-    o = odom.pose.pose.orientation
+    (x,y) = (data.pose.pose.position.x, data.pose.pose.position.y)
+    o = data.pose.pose.orientation
     yaw = tf.transformations.euler_from_quaternion((o.x, o.y, o.z, o.w))[2]
-    odom = (x,y,yaw)
-    print_results()
+    odom.append((data.header.stamp, x,y,yaw))
 
 def wheel_odom_callback(data):
-    odom = data
-    (x,y) = (odom.pose.pose.position.x, odom.pose.pose.position.y)
-    o = odom.pose.pose.orientation
+    (x,y) = (data.pose.pose.position.x, data.pose.pose.position.y)
+    o = data.pose.pose.orientation
     yaw = tf.transformations.euler_from_quaternion((o.x, o.y, o.z, o.w))[2]
-    wheel_odom = (x,y,yaw)
-    print_results()
+    wheel_odom.append((data.header.stamp,x,y,yaw))
 
 def imu_callback(data):
-    imu = data
-    o = imu.orientation
+    o = data.orientation
     yaw = tf.transformations.euler_from_quaternion((o.x, o.y, o.z, o.w))[2]
-    imu = (0,0,yaw)
-    print_results()
+    imu.append((data.header.stamp,yaw))
 
 if __name__ == '__main__':
     rospy.init_node('odometry_debug')
@@ -44,4 +47,15 @@ if __name__ == '__main__':
     rospy.Subscriber('imu_data', Imu, imu_callback)
     rospy.Subscriber('odom', Odometry, odom_callback)
 
-    rospy.spin()
+    pub_cmd_vel = rospy.Publisher('cmd_vel', Twist)
+
+    # move in a straight line forward for one second
+    print "moving in a straight line forward at .5 m/s for 2 sec"
+    t = Twist()
+    t.x = 0.5
+    for i in range(0,20):
+        pub_cmd_vel.publish(t)
+        rospy.sleep(.1)
+    print_results()
+    
+
