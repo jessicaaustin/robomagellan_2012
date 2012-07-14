b170b5d178d251a4c8ba3258a005b8760187b3ae
Bill Mania <maniabill@yahoo.com>
2012-07-14 17:17:41 -0500

    added more output details

--- robomagellan/nodes/motors_calibration.py
+++ robomagellan/nodes/motors_calibration.py
@@ -15,12 +15,15 @@ import math
 
 from sensor_msgs.msg import Imu
 from nav_msgs.msg import Odometry
+from geometry_msgs.msg import Twist
 from tf import transformations
 
 currentX = 0.0
 currentY = 0.0
 currentOdomTheta = 0.0
 currentImuTheta = 0.0
+currentTranslate = 0.0
+currentRotate = 0.0
 
 def handleOdometryMessage(odometryMessage):
     global currentX, currentY, currentOdomTheta
@@ -28,10 +31,10 @@ def handleOdometryMessage(odometryMessage):
     currentX = odometryMessage.pose.pose.position.x
     currentY = odometryMessage.pose.pose.position.y
     orientation = [
-        odometryMessage.pose.orientation.x,
-        odometryMessage.pose.orientation.y,
-        odometryMessage.pose.orientation.z,
-        odometryMessage.pose.orientation.w
+        odometryMessage.pose.pose.orientation.x,
+        odometryMessage.pose.pose.orientation.y,
+        odometryMessage.pose.pose.orientation.z,
+        odometryMessage.pose.pose.orientation.w
         ]
     currentOdomTheta = transformations.euler_from_quaternion(orientation)[2]
 
@@ -51,8 +54,19 @@ def handleImuMessage(imuMessage):
 
     return
 
+def handleTwistMessage(twistMessage):
+    global currentTranslate, currentRotate
+
+    currentTranslate = twistMessage.linear.x
+    currentRotate = twistMessage.angular.z
+
+    return
+
 def displayPoseAndTwist():
-    print "X m: %5.2f, Y m: %5.2f, OdomTheta d: %3d, ImuTheta d: %3d" % (
+    print "%d - T m/s: %5.2f, R d/s: %d X m: %5.2f, Y m: %5.2f, OdomTheta d: %3d, ImuTheta d: %3d" % (
+        rospy.get_time(),
+        currentTranslate,
+        currentRotate * 360 / (2 * math.pi),
         currentX,
         currentY,
         currentOdomTheta  * 360 / (2 * math.pi),
@@ -66,8 +80,9 @@ if __name__ == '__main__':
 
     rospy.Subscriber('imu_data', Imu, handleImuMessage)
     rospy.Subscriber('wheel_odom', Odometry, handleOdometryMessage)
+    rospy.Subscriber('cmd_vel', Twist, handleTwistMessage)
 
-    consistentFrequency = rospy.Rate(1)
+    consistentFrequency = rospy.Rate(4)
     while not rospy.is_shutdown():
         displayPoseAndTwist()
 
