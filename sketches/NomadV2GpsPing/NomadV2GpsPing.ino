e5551a83ba984acde48695606d0a466d360eba0a
Bill Mania <maniabill@yahoo.com>
2013-09-19 08:08:27 -0700

    GPS sketch determines baud rate.

--- sketches/NomadV2GpsPing/NomadV2GpsPing.ino
+++ sketches/NomadV2GpsPing/NomadV2GpsPing.ino
@@ -22,54 +22,82 @@ calcChecksum(
     static unsigned char checksum;
     static char checksumString[2];
 
+    Serial.print("Checksum> ");
+
     checksum = (unsigned char) 0;
-    while (messageLength > 0) {
-        checksum ^= *message;
-        messageLength--;
+    for (int index = 0; index < messageLength; index++) {
+        Serial.print(message[index]); Serial.print(" ");
+        checksum ^= message[index];
     };
 
     sprintf(checksumString, "%02X", checksum);
-    /*
-    Serial.print("C>");
     Serial.println(checksumString);
-     */
 
     return checksumString;
 }
 
 void
+findDataRate() {
+	long dataRates[] = { 4800, 9600, 14400, 19200, 38400, 57600, 115200 };
+
+    gpsDevice.setTimeout((unsigned long) 1000);
+
+    for (int index = 0; index < 7; index++) {
+        Serial.print("Trying ");
+        Serial.println(dataRates[index]);
+
+        gpsDevice.begin(dataRates[index]);
+        gpsDevice.println("$PMTK000*32");
+        bytesRead = gpsDevice.readBytes(inputBuffer, 255);
+        inputBuffer[bytesRead] = 0;
+        Serial.print("Read> ");
+        Serial.println(inputBuffer);
+        if (strstr(inputBuffer, "$PMTK001") != NULL) {
+            Serial.print("Found data rate ");
+            Serial.println(dataRates[index]);
+            break;
+        };
+        delay(500);
+    };
+
+    return;
+};
+
+void
+sendSentence(char *message) {
+    gpsDevice.print("$");
+    gpsDevice.print(message);
+    gpsDevice.print("*");
+    gpsDevice.println(calcChecksum(message, strlen(message)));
+
+    return;
+};
+
+void
 setup() {
     Serial.begin(115200);
     Serial.println("GPS sketch 1");
-    Serial.println("Setting data rate to 9600");
-    gpsDevice.begin(115200);
-    gpsDevice.setTimeout((unsigned long) 1000);
     
-    /*
-     * - set the baud rate
-     */
-    gpsDevice.println("$PMTK251,9600*17");
-    gpsDevice.end();
-    gpsDevice.begin(9600);
-    gpsDevice.flush();
+    findDataRate();
+    delay(10000);
 
     /*
      * - select the NMEA sentences - GPGGA only
      */
     Serial.println("Only the GPGGA sentence");
-    gpsDevice.println("$PMTK314,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0*29");
+    sendSentence("PMTK314,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0");
 
     /*
      * - set the update frequency - once per second
      */
     Serial.println("Set update frequency to 1 Hz");
-    gpsDevice.println("$PMTK220,1000*1F");
+    sendSentence("PMTK220,1000");
 
     /*
      * - enable WAAS
      */
     Serial.println("Enable WAAS");
-    gpsDevice.println("$PMTK301,2*2E");
+    sendSentence("PMTK301,2");
     
     sentence[sentenceIndex] = eostr;
     return;
