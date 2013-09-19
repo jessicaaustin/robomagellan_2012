d7e0e6dcb4d4a0353f4dc40ab0843f9b144a6215
Bill Mania <maniabill@yahoo.com>
2013-09-18 19:04:14 -0700

    Debugging and testing the Gps sketch.

--- sketches/NomadV2GpsPing/NomadV2GpsPing.ino
+++ sketches/NomadV2GpsPing/NomadV2GpsPing.ino
@@ -9,8 +9,8 @@ uint8_t receivePin = 3,
         sentenceIndex = 0;
 SoftwareSerial gpsDevice(receivePin, transmitPin);
 char sentence[MAX_SENTENCE_LENGTH + 1];
-char byteRead;
-char emptyBuffer = ((char) -1);
+char inputBuffer[256];
+int bytesRead;
 char eostr = ((char) 0);
 
 char*
@@ -29,8 +29,10 @@ calcChecksum(
     };
 
     sprintf(checksumString, "%02X", checksum);
+    /*
     Serial.print("C>");
     Serial.println(checksumString);
+     */
 
     return checksumString;
 }
@@ -38,7 +40,9 @@ calcChecksum(
 void
 setup() {
     Serial.begin(115200);
-    gpsDevice.begin(9600);
+    Serial.println("GPS sketch 1");
+    Serial.println("Setting data rate to 9600");
+    gpsDevice.begin(115200);
     gpsDevice.setTimeout((unsigned long) 1000);
     
     /*
@@ -52,16 +56,19 @@ setup() {
     /*
      * - select the NMEA sentences - GPGGA only
      */
+    Serial.println("Only the GPGGA sentence");
     gpsDevice.println("$PMTK314,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0*29");
 
     /*
      * - set the update frequency - once per second
      */
+    Serial.println("Set update frequency to 1 Hz");
     gpsDevice.println("$PMTK220,1000*1F");
 
     /*
      * - enable WAAS
      */
+    Serial.println("Enable WAAS");
     gpsDevice.println("$PMTK301,2*2E");
     
     sentence[sentenceIndex] = eostr;
@@ -70,36 +77,14 @@ setup() {
 
 void
 readGpsSentence() {
-    for (;;) {
-        byteRead = gpsDevice.read();
-        if (byteRead == emptyBuffer) {
-            break;
-        };
-    
-        switch(byteRead) {
-    
-        case SENTENCE_END :
-            sentence[sentenceIndex] = byteRead;
-            sentenceIndex++;
-            sentence[sentenceIndex] = eostr;
-            Serial.println(sentence);
-            sentenceIndex = 0;
-            return;
-    
-        default   : 
-            if (byteRead < 32) {
-                break;
-            }
-            if (byteRead > 126) {
-                break;
-            }
-    
-            sentence[sentenceIndex] = byteRead;
-            sentenceIndex++;
-            break;
-        }
-    
+    bytesRead = gpsDevice.readBytes(inputBuffer, 255);
+    if (bytesRead == 0) {
+        Serial.println("No bytes read");
+    } else {
+        inputBuffer[bytesRead] = (char) 0;
+        Serial.print(inputBuffer);
     };
+        
 };
 
 void
